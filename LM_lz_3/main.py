1a5e71e0a437497df17e67d0513ba856542f5b35
Kuga09 <glal64@tutamail.com>
2025-11-14 13:49:06 +0300

    1

--- LM_lz_3/main.py
+++
@@ -1,8 +0,0 @@
-from programm import model_mnist, my_image
-
-if __name__ == "__main__":
-    # Обучаем модель
-    model = model_mnist()
-
-    # Проверяем на сторонней картинке
-    my_image(model, "cifra.png")
